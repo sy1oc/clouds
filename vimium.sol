bf5ee0f2b14d64730ad355c0fa790a5e466d827c
sy1oc <2816135275@qq.com>
2025-05-08 15:29:28 +0800

    vimium

---
+++ vimium.sol
@@ -0,0 +1,20 @@
+# Don't forget to change the new tab url into google.com
+unmap h
+unmap H
+unmap W
+map L Vomnibar.activateInNewTab
+map l Vomnibar.activate
+unmap k
+unmap d
+unmap u
+unmap K
+map n previousTab
+map o nextTab
+map N goBack
+map O goForward
+map j performFind
+map J performBackwardsFind
+map E scrollPageDown
+map I scrollPageUp
+map e scrollDown
+map i scrollUp
